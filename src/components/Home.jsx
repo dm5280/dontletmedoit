1035f666a38243e4d45bf5596a7243b18b146627
deep <egor.grachev.mail@gmail.com>
2022-07-18 17:07:59 +0300

    added mint function

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,7 @@
 import React, { useEffect, useState } from 'react'
 
 import { ethers } from 'ethers';
-import { useWeb3React } from "@web3-react/core"
-import { injected } from "../components/wallet/connectors"
 import abi from "../components/wallet/dontletmedoit.json"
-import { Contract } from "@ethersproject/contracts";
 
 import Install from './body/Install';
 import Connect from './body/Connect';
@@ -17,50 +14,32 @@ import dis from './images/dis.png';
 import tw from './images/twitter.png';
 
 function Home() {
-    // const { active, account, connector, activate, deactivate } = useWeb3React();
+    const [adress, setAdress] = useState();
     const contractAddress = '0x3ba2cab27c660999cd6f06e9f2cf5a1518ad3cc4';
     const [isCountdown, setStartCountdown] = useState(true);
-    let time = 0;
-    // const provider = library.getSigner()
-
-    // const contract = new Contract(contractAddress, abi, provider)
 
     async function connect() {
-        // try {
-        //     await activate(injected)
-        //     localStorage.setItem('isWalletConnected', true)
-        // } catch (ex) {
-        //     console.log(ex)
-        // }
-    }
-
-    async function disconnect() {
-        // try {
-        //     deactivate()
-        //     localStorage.setItem('isWalletConnected', false)
-        // } catch (ex) {
-        //     console.log(ex)
-        // }
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const accounts = await provider.send("eth_requestAccounts", [])
+        setAdress(accounts[0])
     }
 
     async function mint(quantity) {
-        console.log(quantity)
-        // try {
-        //     const tx = await contract.mint(1);
-        //     await tx.wait();
-        // } catch (error) {
-        //     console.log(error);
-        // }
-    }
+        try {
+            const provider = new ethers.providers.Web3Provider(window.ethereum);
+            const signer = provider.getSigner();
+            const contract = new ethers.Contract(contractAddress, abi, signer);
 
-    // async function test() {
-    //     try {
-    //         const tx = await contract.isActive()
-    //         console.log(tx);
-    //     } catch (error) {
-    //         console.log(error);
-    //     }
-    // }
+            const connection = contract.connect(signer);
+            const addr = connection.address;
+
+            const result = await contract.mint(quantity)
+
+            await result.wait();
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
     const [unixTime, setUnixTime] = React.useState();
 
@@ -72,18 +51,6 @@ function Home() {
 
     React.useEffect(() => {
         getTime();
-
-        // const connectWalletOnPageLoad = async () => {
-        //     if (localStorage?.getItem('isWalletConnected') === 'true') {
-        //       try {
-        //         await activate(injected)
-        //         localStorage.setItem('isWalletConnected', true)
-        //       } catch (ex) {
-        //         console.log(ex)
-        //       }
-        //     }
-        //   }
-        //   connectWalletOnPageLoad()
     }, []);
     
     return (
@@ -94,7 +61,7 @@ function Home() {
                 {isCountdown
                     ?  <Timer unixTime={unixTime} />
                     : (
-                    1
+                    adress
                         ? <Mint mint={mint} />
                         : <Connect connectToWallet={connect} />
                         // : <Sold />
@@ -107,8 +74,7 @@ function Home() {
                 <p className='font-mont my-10 text-md md:text-sm sm:text-xs'>
                     We use the ERC-721A standard for our <a href='#' className='underline hover:no-underline'>smart contract</a><br />
                     to make your gas fee as funny as our project too<br /><br />
-                    {!1 && <span className='text-xs sm:text-md'>Your wallet: <br />
-                    <a onClick={disconnect} className='underline hover:no-underline cursor-pointer'>Disconnect</a></span>}
+                    {adress && <span className='text-xs sm:text-md'>Your wallet: {adress}</span>}
                 </p>
             </div>
         </div>
